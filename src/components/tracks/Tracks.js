2f600d8893b2b6599834b2bffb6a6569abaf92ff
jovanka <jovanka.placeres@gmail.com>
2018-11-27 07:50:33 +0000

    added to logic else statement; added to destructure to snag heading state as well; once data is fetched, maps over each track being passed into track prop to be used in Track component file

--- src/components/tracks/Tracks.js
+++ src/components/tracks/Tracks.js
@@ -1,19 +1,29 @@
 import React, { Component } from 'react';
 import { Consumer } from '../../context';
 import Spinner from '../layouts/Spinner';
+import Track from '../tracks/Track';
 // class component
 class Tracks extends Component {
 	render() {
 		return (
 			<Consumer>
 				{value => {
-					const { track_list } = value;
+					const { track_list, heading } = value;
 					
 					if(track_list === undefined || track_list.length === 0) {
 						return <Spinner/>
 					}
 					else {
-						return <h1>Tracks Loaded</h1>;
+						return (
+							<React.Fragment>
+							<h3 className='text-center mb-4'>{heading}</h3>
+							<div className='row'>
+								{track_list.map((info) => (
+									<Track key={info.track.track_id} track={info.track}/>
+								))}
+							</div>
+							</React.Fragment>
+						)
 					}
 				}}
 			</Consumer>
