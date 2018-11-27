590e2e859b3f226b0ad593ee19052c2ec1a35237
jovanka <jovanka.placeres@gmail.com>
2018-11-27 06:12:25 +0000

    imports consumer from context.js file; Context.Consumer; component returned from within Tracks class component

--- src/App.js
+++ src/App.js
@@ -5,19 +5,24 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/layouts/Navbar';
 import Index from './components/layouts/Index';
 
+// in curly braces, not a default export
+import { Provider } from './context'
+
 class App extends Component {
   render() {
     return (
-      <Router>
-      <React.Fragment>
-        <Navbar />
-        <div className='container'>
-          <Switch>
-            <Route exact path='/' component={Index} />
-          </Switch>
-        </div>
-      </React.Fragment>
-      </Router>
+      <Provider>
+        <Router>
+        <React.Fragment>
+          <Navbar />
+          <div className='container'>
+            <Switch>
+              <Route exact path='/' component={Index} />
+            </Switch>
+          </div>
+        </React.Fragment>
+        </Router>
+      </Provider>
     );
   }
 }
--- src/components/tracks/Tracks.js
+++ src/components/tracks/Tracks.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
-
+import { Consumer } from '../../context';
 // class component
 class Tracks extends Component {
 	render() {
 		return (
-			<div>
-				<h1>Tracks</h1>
-			</div>
+			<Consumer>
+				{value => {
+					console.log(value);
+					return <h1>Tracks</h1>
+				}}
+			</Consumer>
 		)
 	}
 }
