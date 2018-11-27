1cc2b6cddcfca9a4a0697b39eca6afd234e0878c
jovanka <jovanka.placeres@gmail.com>
2018-11-27 02:29:25 +0000

    deleted css pulled with create-react-app; kept file to customize

---
+++ src/components/layouts/Index.js
@@ -0,0 +1,14 @@
+// this component file will hold components for API
+import React from 'react';
+import Tracks from '../tracks/Tracks';
+
+// functional component
+const Index = () => {
+	return (
+		<React.Fragment>
+			<Tracks />
+		</React.Fragment>
+	)
+}
+
+export default Index;
---
+++ src/components/layouts/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// functional component
+const Navbar = () => {
+	return (
+		<nav className='navbar navbar-dark bg-dark mb-5'>
+			<span className='navbar-brand mb-0 h1 mx-auto'>lyrics <i class='fas fa-music'></i> finder</span>
+		</nav>
+	)
+}
+
+export default Navbar;
