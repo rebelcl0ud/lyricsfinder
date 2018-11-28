e31cd1b157d0f74e6cab2bcbe8a94264682f8733
jovanka <jovanka.placeres@gmail.com>
2018-11-28 21:59:12 +0000

    added dispatch to state; dispatch will take an action that will call a reducer fn that will then update state with Provider; this state change will be used to update display of top 10 tracks and accompanying heading; the reducer fn will check for action type which will return changed state dependent on case-- SEARCH_TRACKS would return state; update track_list with action payload and heading ref data displayed/ w.e track was searched

--- src/context.js
+++ src/context.js
@@ -8,16 +8,30 @@ Provider will wrap around every other component
 w.e state put in this Provider will be able to be accessed using 'consumer' 
 **/
 
+const reducer = (state, action) => {
+	switch (action.type) {
+		case 'SEARCH_TRACKS':
+			return {
+				...state,
+				track_list: action.payload,
+				heading: 'Search Results'
+			}
+			default:
+			return state;
+	}
+}
+
 export class Provider extends Component {
 	state = {
 		track_list: [],
 		heading: 'Top 10 Tracks',
+		dispatch: action => this.setState(state => reducer(state, action))
 	}
 
 	componentDidMount() {
 		axios.get(`https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/chart.tracks.get?chart_name=top&page=1&page_size=10&country=us&f_has_lyrics=1&apikey=${process.env.REACT_APP_MMKEY}`)
 			.then((res) => {
-				console.log(res.data);
+				// console.log(res.data);
 				this.setState({track_list: res.data.message.body.track_list})
 			})
 			.catch((err) => {
