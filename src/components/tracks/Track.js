f96407075527e15f8a5b092bc95750c1cde6a44b
jovanka <jovanka.placeres@gmail.com>
2018-11-28 04:11:45 +0000

    troubleshooting API call; typo template string for link/ view lyrics button

--- src/components/tracks/Track.js
+++ src/components/tracks/Track.js
@@ -14,7 +14,7 @@ const Track = (props) => {
 						<br />
 						<strong><i className='fas fa-compact-disc'></i> Album</strong>: {track.album_name}
 					</p>
-					<Link to={`lyrics/track/{track.track_id}`} className='btn btn-dark btn-block'>
+					<Link to={`lyrics/track/${track.track_id}`} className='btn btn-dark btn-block'>
 						<i className='fas fa-chevron-right'></i> view lyrics
 					</Link>
 				</div>
