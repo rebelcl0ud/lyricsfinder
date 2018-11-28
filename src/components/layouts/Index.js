62d3ed9d017ed154ae87ae2295600ef6b1525e30
jovanka <jovanka.placeres@gmail.com>
2018-11-28 06:42:11 +0000

    import of Search component into Index file which is root/homepage; added Search component to be returned as with Tracks

--- src/components/layouts/Index.js
+++ src/components/layouts/Index.js
@@ -1,11 +1,13 @@
 // this component file will hold components for API
 import React from 'react';
 import Tracks from '../tracks/Tracks';
+import Search from '../tracks/Search';
 
 // functional component
 const Index = () => {
 	return (
 		<React.Fragment>
+			<Search />
 			<Tracks />
 		</React.Fragment>
 	)
